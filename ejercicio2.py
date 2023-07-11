44824bdf44c8002ab05312decf4c9554727ab6fc
camilorey123 <cristian2221475@correo.uis.edu.co>
2023-07-11 10:25:16 -0500

    actualizar

---
+++ ejercicio2.py
@@ -0,0 +1,14 @@
+
+class carta :
+   def __init__(self, tipo, nombre):
+        self.tipo = tipo
+        self.nombre = nombre 
+
+    def __str__(self):
+        return f"{self.tipo} de {self.nobre }"
+
+class mazo:
+    pass
+
+class jugador:
+    pass
